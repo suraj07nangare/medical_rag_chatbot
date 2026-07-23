3e105d8b705d9b1aae6be17682bc3fee568ab5d8
Suraj <surajnangare1010@email.com>
2026-07-23 06:32:32 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,15 @@
+#create directory 
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trial.ipynb
+touch requirement.txt
+
+echo "Directory and files are created"
